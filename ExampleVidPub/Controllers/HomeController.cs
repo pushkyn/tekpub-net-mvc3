9f3970dcc621fc1f1970ebc7687a0bf6bcd3d2a4
pushkyn <alex.pushkyn@gmail.com>
2014-01-27 22:32:21 +0400

    added the Nlog and Ninject libraries

--- ExampleVidPub/Controllers/HomeController.cs
+++ ExampleVidPub/Controllers/HomeController.cs
@@ -1,15 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using ExampleVidPub.Infrastructure.Logging;
 
 namespace ExampleVidPub.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger _logger;
+
+        public HomeController(ILogger logger)
+        {
+            _logger = logger;
+        }
+
         public ActionResult Index()
         {
+            _logger.LogInfo("Hey - I called the Home page");
             ViewBag.Message = "Welcome to ASP.NET MVC!";
 
             return View();
---
+++ ExampleVidPub/Infrastructure/Logging/ILogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExampleVidPub.Infrastructure.Logging
+{
+    public interface ILogger
+    {
+        void LogInfo(string message);
+        void LogWarning(string message);
+        void LogDebug(string message);
+        void LogError(string message);
+        void LogError(Exception x);
+        void LogFatal(string message);
+        void LogFatal(Exception x);
+    }
+}
